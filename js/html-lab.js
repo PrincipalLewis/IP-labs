004f2a5bb69ff56f0efe0c5867d28999e5656f8e
Vazyzy <igorkorshynov@gmail.com>
2015-05-02 18:14:20 +0300

    menu

--- js/html-lab.js
+++ js/html-lab.js
@@ -4,12 +4,16 @@
 
 function loadExample(id) {
     $('#main').load('html-lab/Ex' + id + '.html');
-    console.log('!!!!!!!!!!!!!!!!!!!!');
-    var main = document.getElementById('main');
-    main.innerHTML += '<p >Задание' + id + '</p>';
+    showBackButton();
 }
 
 
 function openMainHtmlLab() {
     $('#main').load('html-lab/main.html');
+    document.getElementById("back-button").style.visibility = "hidden";
+}
+
+
+function showBackButton() {
+    document.getElementById("back-button").style.visibility = "visible";
 }
