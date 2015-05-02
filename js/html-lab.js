4f8aec748a18efe557a289a5c9970af10a5a2ef9
Vazyzy <igorkorshynov@gmail.com>
2015-05-02 17:01:24 +0300

    main html

---
+++ js/html-lab.js
@@ -0,0 +1,10 @@
+//var bootstrap = require('bootstrap.js');
+
+
+$('#open-html-labs').click(function () {
+    $('#main').load('html-lab/main.html');
+});
+
+$('#html-lab1').click(function () {
+    $('#main').load('html-lab/Ex1.html');
+});
