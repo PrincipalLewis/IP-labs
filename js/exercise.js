5b1f154b2543a4f7b4f5e2544123b54973b8708c
PrincipalLewis <dflz666@gmail.com>
2015-05-02 18:12:59 +0300

    js2

--- js/exercise.js
+++ js/exercise.js
@@ -14,6 +14,24 @@ function task1 () {
     document.getElementById("main").appendChild(table);
 }
 
+function task2() {
+    document.getElementById("main").innerHTML = '<img id="img" src="img/img1.gif" width=200 height=200>';
+    var angel = 180;
+    $( "#img" ).mouseover(function() {
+        this.style.webkitTransform='rotate(180deg)';
+        this.style.mozTransform='rotate(180deg)';
+        this.style.oTransform='rotate(180deg)';
+        this.style.msTransform='rotate(180eg)';
+        this.style.transform='rotate(180deg)';
+    });
+    $( "#img" ).mouseout(function() {
+        this.style.mozTransform='rotate(0)';
+        this.style.oTransform='rotate(0)';
+        this.style.msTransform='rotate(0)';
+        this.style.transform='rotate(0)';
+    });
+}
+
 $('#js-lab1').click(function () {
-    task1();
+    task2();
 });
